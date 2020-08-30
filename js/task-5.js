bd471f0a4cf8609b94c89d9b2fd065296be08f3a
Dmitriy Galochkin <arenda.tehniki87@gmail.com>
2020-08-30 09:51:21 +0300

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,17 +1,33 @@
-function checkForSpam (str) { 
-    'use strict';
-    // Write code under this line
-         
+function getAllPropValues (array, prop) {
+  'use strict';
+  // Write code under this line 
+  const result = [];
+  
+  for(const product of array) {
+  	if(prop in product){
+    result.push(product[prop])
+    } 
       
-    return str.toLowerCase().includes('spam') || str.toLowerCase().includes('sale') ? true : false;
-    
-    
   }
-  
-  console.log(checkForSpam('Latest technology news')); // false
-  
-  console.log(checkForSpam('JavaScript weekly newsletter')); // false
-  
-  console.log(checkForSpam('Get best sale offers now!')); // true
-  
-  console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+  return result;
+}
+
+// Объекты и ожидаемый результат
+const products = [
+    { name: 'Радар', price: 1300, quantity: 4 },
+    { name: 'Радар', price: 1280, quantity: 2 },
+    { name: 'Радар', price: 1320, quantity: 1 },
+    { name: 'Сканер', price: 2700, quantity: 1 },
+    { name: 'Сканер', price: 2500, quantity: 3 },
+    { name: 'Дроид', price: 400, quantity: 7 },
+    { name: 'Захват', price: 1200, quantity: 2 },
+]; 
+
+console.log(getAllPropValues(products, 'name'));
+// ['Радар', 'Радар', 'Радар', 'Сканер', 'Сканер', 'Дроид', 'Захват']
+
+console.log(getAllPropValues(products, 'quantity'));
+// [4, 2, 1, 1, 3, 7, 2]
+
+console.log(getAllPropValues(products, 'category'));
+//  []
